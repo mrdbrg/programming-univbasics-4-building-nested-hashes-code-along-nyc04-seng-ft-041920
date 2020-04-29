ceabe3a5fdb6257229c527b9885afbc2824d89e8
Marlon Braga <rdg.brg@gmail.com>
2020-04-29 20:36:15 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -3,7 +3,7 @@ def second_challenge
     :montague => {
       :patriarch => {
         :name => "Lord Montague",
-        :
+        :age => ""
       },
       :matriarch => {},
       :hero => {},
