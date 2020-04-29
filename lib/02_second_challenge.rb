81deddcdecee046cab5d3d9817754da117dd1dd1
Marlon Braga <rdg.brg@gmail.com>
2020-04-29 20:35:53 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,7 +1,9 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {},
+      :patriarch => {
+        :name =>
+      },
       :matriarch => {},
       :hero => {},
       :hero_friends => []
