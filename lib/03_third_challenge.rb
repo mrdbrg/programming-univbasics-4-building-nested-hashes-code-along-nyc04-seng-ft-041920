84d29f1b43d55369a4d0b7e56de5638b6d711912
Marlon Braga <rdg.brg@gmail.com>
2020-04-29 20:52:30 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -14,7 +14,18 @@ def third_challenge
         :age => "15",
         :status => "alive"
       },
-      :hero_friends => []
+      :hero_friends => [
+        {
+          :name => "Benvolio",
+          :age => "17",
+          :attitude => "worried"
+        },
+        {
+          :name => "Mercutio"
+          :age =>
+          :attitude => 
+        }
+      ]
    }, 
    :capulet => {
       :patriarch => {
