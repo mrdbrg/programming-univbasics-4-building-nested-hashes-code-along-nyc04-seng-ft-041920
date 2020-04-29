0f6214286e07e9d93bfeb93b30eb54bef7332fdf
Marlon Braga <rdg.brg@gmail.com>
2020-04-29 20:42:12 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,21 +1,7 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {
-        :name => "Lord Montague",
-        :age => "53"
-      },
-      :matriarch => {
-        :name => "Lady Montague",
-        :age => "54"
-      },
-      :hero => {
-        :name => "Romeo",
-        :age => "15"
-      },
-      :hero_friends => [
-        
-        ]
+    
       }, 
     :capulet => {
       :patriarch => {},
